d479d2b3c5674ba1c73a743ac2340545e900a3c3
YC-910 <144610099+YC-910@users.noreply.github.com>
2026-04-27 20:03:24 +0800

    Update code_only.py

--- code_only.py
+++ code_only.py
@@ -143,16 +143,16 @@ def save_notes(df):
 
     for _, row in df.iterrows():
         cursor.execute("""
-            INSERT INTO "Notes" (id, date, title, content, tags, user_id)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """,
-        int(row["id"]),
+    INSERT INTO "Notes" (id, date, title, content, tags, user_id)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (
+        new_id,
         row["date"],
         row["title"],
         row["content"],
-        clean(row.get("tags")),
+        row.get("tags"),
         st.session_state.user_db_id
-        )
+    ))
 
     conn.commit()
     conn.close()
