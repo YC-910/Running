0189846be61715c661107163d63a9631f0fda64e
YC-910 <144610099+YC-910@users.noreply.github.com>
2026-05-12 18:24:56 +0800

    Update code_only.py

--- code_only.py
+++ code_only.py
@@ -458,15 +458,6 @@ def delete_event(event_id):
     conn.commit()
     conn.close()
 
-hide_github_icon = """
-<style>
-#GithubIcon {
-  visibility: hidden;
-}
-</style>
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 # ==================================================
 # ULTRA-PREMIUM GARMIN / STRAVA CSS (MOBILE FRIENDLY)
 # ==================================================
